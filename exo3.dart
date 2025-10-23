fa0f95b46fb95de5cf085515dc8b56d5e75911ff
amelzourane <amelzourane@monboreal.ca>
2025-10-22 22:59:11 -0400

    Ex3 Q5: ajout getter solde formaté

--- exo3.dart
+++ exo3.dart
@@ -2,7 +2,8 @@ class Compte {
   double _solde = 0.0; 
   final String numerCompte;
   Compte(this.numerCompte, [this._solde = 0.0]);
-
+  String get solde => "${_solde.toStringAsFixed(2)} \$";
+  
 
 
 }
