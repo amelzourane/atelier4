6ad7ef243b7b04300b9e5b79e1f8ec94b71cf453
amelzourane <amelzourane@monboreal.ca>
2025-10-22 22:39:07 -0400

    Ex2 Q4: Ajout de la méthode display()

--- exo2.dart
+++ exo2.dart
@@ -2,5 +2,12 @@ class Point {
   double _x;
   double _y;
   Point(this._x, this._y);
+  void move(double dx, double dy) {
+    _x += dx;
+    _y += dy;
+  }
+  void display() {
+    print("Point: (x = $_x, y = $_y)");
+  }
 
 }
