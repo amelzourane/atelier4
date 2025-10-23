7628460218ba58a8a9a4cd23ae98e41c3021420e
amelzourane <amelzourane@monboreal.ca>
2025-10-22 23:00:49 -0400

    Ex3 Q6: ajout méthode depot

--- exo3.dart
+++ exo3.dart
@@ -3,7 +3,15 @@ class Compte {
   final String numerCompte;
   Compte(this.numerCompte, [this._solde = 0.0]);
   String get solde => "${_solde.toStringAsFixed(2)} \$";
-  
-
-
+   void depot(double montant) {
+    if (montant > 0) {
+      _solde += montant;
+      print("Dépôt de $montant \$ effectué !");
+    } else {
+      print("Le montant doit être positif.");
+    }
+  }
 }
+
+
+
