dc9c56587191e6fc6f370572b1257fccb4375f56
amelzourane <amelzourane@monboreal.ca>
2025-10-24 20:45:40 -0400

    Ex6 Q9: Ajout de main avec exécution connecter() et deconnecter() sur chaque service

--- exo6.dart
+++ exo6.dart
@@ -11,3 +11,23 @@ class ServeurAPI implements Connectable {
     print("ServeurAPI : Déconnexion réussie.");
   }
 }
+class BaseDeDonnees implements Connectable {
+  void connecter(String utilisateur) {
+    print("BaseDeDonnees : Connexion établie pour $utilisateur.");
+  }
+
+  void deconnecter() {
+    print("BaseDeDonnees : Déconnexion réussie.");
+  }
+}
+void main() {
+  List<Connectable> services = [
+    ServeurAPI(),
+    BaseDeDonnees(),
+  ];
+
+  for (var s in services) {
+    s.connecter("Amel");
+    s.deconnecter();
+  }
+}
