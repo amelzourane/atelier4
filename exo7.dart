a115d21e694ddc7b8580a430068a51221dc40694
amelzourane <amelzourane@monboreal.ca>
2025-10-24 21:25:57 -0400

    Ex7 Q5: Ajout de main() avec création des tâches et affichage du nombre total

--- exo7.dart
+++ exo7.dart
@@ -5,3 +5,10 @@ class Tache {
     nombreTotal++;
   }
 }
+void main() {
+var T1 = Tache("Faire les courses");
+var T2 = Tache("Réviser pour l'examens");
+var T3 = Tache("Aller au sport");
+
+  print("Nombre total de tâches : ${Tache.nombreTotal}");
+}
